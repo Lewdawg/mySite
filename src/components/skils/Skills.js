c76fd4df3918aeff20ba7e3bd01e143d6b4c1d62
Lewdawg <Leweswalton@gmail.com>
2021-04-07 18:51:56 +0200

    more hover effects

--- src/components/skils/Skills.js
+++ src/components/skils/Skills.js
@@ -21,24 +21,46 @@ function Skills() {
             <h1>SKILLS</h1>
 
             <div className="logos">
+
                 <div>
                     <img src={html} alt="" />
                     <p>HTML 5</p>
                 </div>
+
                 <div>
                     <img src={js} alt="" />
+                    <p>JavaScript</p>
                 </div>
-                <img src={node} alt="" />
-                <img src={react} alt="" />
+
+                <div>
+                    <img src={node} alt="" />
+                    <p>Node.js</p>
+                </div>
+
+                <div>
+                    <img src={react} alt="" />
+                    <p>React</p>
+                </div>
+
             </div>
 
             <div className="logos">
                 <div>
                     <img src={css} alt="" />
+                    <p>CSS 3</p>
                 </div>
-                <img src={mongo} alt="" />
-                <img src={git} alt="" />
-                <img src={express} alt="" />
+                <div>
+                    <img src={mongo} alt="" />
+                    <p>MongoDB</p>
+                </div>
+                <div>
+                    <img src={git} alt="" />
+                    <p>Git</p>
+                </div>
+                <div>
+                    <img src={express} alt="" />
+                    <p>Express</p>
+                </div>
             </div>
 
         </div>
