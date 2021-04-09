903e1275ba3e65c3cbdbe469759aabb849cce912
Lewdawg <Leweswalton@gmail.com>
2021-04-09 15:23:17 +0200

    some more res design

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,6 +8,22 @@ function Navbar() {
 
         <div className="navbar">
 
+            <div class="dropdown" style={{ 'right': '0' }}>
+
+                <button class="dropbtn">Menu</button>
+
+                <div class="dropdown-content">
+
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#contact">Contact</a>
+
+                </div>
+            </div>
+
+            {/**********************/}
+
             <div className="links">
 
                 <a href="#home">Home</a>
